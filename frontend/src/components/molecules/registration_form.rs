e62c9ca47208332b6b0b8bb519c6982897c25b45
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2025-01-22 18:51:26 +1100

    Basic registration page

---
+++ frontend/src/components/molecules/registration_form.rs
@@ -0,0 +1,85 @@
+use std::ops::Deref;
+
+use common::AccountRequest;
+use gloo::console::log;
+use yew::prelude::*;
+
+use crate::components::atoms::{button::Button, input_field::InputField};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub on_submit: Callback<Result<AccountRequest, ()>>,
+}
+
+#[derive(Default, Clone)]
+pub struct Form {
+    pub username: String,
+    pub password: String,
+    pub password_confirm: String
+}
+
+#[function_component(RegistrationForm)]
+pub fn registration_form(props: &Props) -> Html {
+    let form_state = use_state(|| Form::default());
+
+    let passwords_match = form_state.password.eq(&form_state.password_confirm);
+
+    let username_changed = {
+        let username_state = form_state.clone();
+        Callback::from(move |text: String| {
+            let mut updated_state = username_state.deref().clone();
+            updated_state.username = text;
+            username_state.set(updated_state);
+        })
+    };
+
+    let password_changed = {
+        let form_state = form_state.clone();
+        Callback::from(move |text: String| {
+            let mut updated_form = form_state.deref().clone();
+            updated_form.password = text;
+            form_state.set(updated_form);
+        })
+    };
+
+    let password_confirm_changed = {
+        let form_state = form_state.clone();
+        Callback::from(move |text: String| {
+            let mut updated_form = form_state.deref().clone();
+            updated_form.password_confirm = text;
+            form_state.set(updated_form);
+        })
+    };
+
+    let on_submit = {
+        let props_on_submit = props.on_submit.clone();
+        let form_state = form_state.clone();
+        Callback::from(move |event: SubmitEvent| {
+            event.prevent_default();
+
+            match !form_state.password.is_empty() && passwords_match {
+                true  => {
+                    let username = form_state.username.clone();
+                    let password = form_state.password.clone();
+                    props_on_submit.emit(Ok(AccountRequest { username, password }))
+                },
+                false => {
+                    log!("passwords are empty or are different");
+                    props_on_submit.emit(Err(()))
+                },
+            }
+        })
+    };
+
+    html! {
+        <form onsubmit={on_submit}>
+            <InputField name="username" password=false autofocus=true on_change={username_changed} />
+            <br />
+            <InputField name="password" password=true autofocus=false on_change={password_changed} />
+            <br />
+            <InputField name="confirm password" password=true autofocus=false on_change={password_confirm_changed} />
+            <br />
+            <Button label="Register" />
+        </form>
+    }
+}
--- frontend/src/pages/registration.rs
+++ frontend/src/pages/registration.rs
@@ -1,11 +1,55 @@
+use std::ops::Deref;
+
+use common::AccountRequest;
+use gloo::console::log;
 use yew::prelude::*;
+
+use crate::{api_service, components::molecules::registration_form::RegistrationForm};
+
+#[derive(PartialEq)]
+enum RegisterStatus {
+    NotAttempted,
+    Success,
+    Failed
+}
 
 #[function_component(RegistrationPage)]
 pub fn registration_page() -> Html {
+    // Component state
+    let status = use_state(|| RegisterStatus::NotAttempted);
+    let render_status = status.clone();
+
+    let on_submit = {
+        Callback::from(move |valid_form: Result<AccountRequest, ()>| {
+            let status = status.clone();
+            match valid_form {
+                Ok(acc_req) => wasm_bindgen_futures::spawn_local(async move {
+                    match api_service::account_register(acc_req).await {
+                        Ok(()) => status.set(RegisterStatus::Success),
+                        Err(e) => {
+                            log!(format!("Registration failed: {}", e));
+                            status.set(RegisterStatus::Failed)
+                        }
+                    }
+                }),
+                Err(_) => {}  // do nothing
+            }
+        })
+    };
+
+    let status_msg = match render_status.deref() {
+        RegisterStatus::NotAttempted => None,
+        RegisterStatus::Success      => Some("Success"),
+        RegisterStatus::Failed       => Some("Failed"),
+    };
+
     html! {
         <div>
             <h1>{ "registration page" }</h1>
-            <h2>{ "TODO: placeholder text" }</h2>
+            <RegistrationForm on_submit={on_submit} />
+            if let Some(msg) = status_msg {
+                <p>{msg}</p>
+            }
         </div>
     }
 }
