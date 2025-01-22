18a080125dff6ec3e3b440e276399e84c5d4227e
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2025-01-22 15:28:07 +1100

    Add optional onclick callback to button.

--- frontend/src/components/atoms/button.rs
+++ frontend/src/components/atoms/button.rs
@@ -1,14 +1,25 @@
 use yew::prelude::*;
 
-#[derive(Properties, PartialEq)]
+#[derive(Properties, PartialEq, Clone)]
 pub struct Props {
     pub label: String,
+    #[prop_or_default]
+    pub on_click: Option<Callback<MouseEvent>>
 }
 
 #[function_component(Button)]
 pub fn button(props: &Props) -> Html {
+    let on_click_callback = {
+        let props_on_click = props.on_click.clone();
+        Callback::from(move |event: MouseEvent| {
+            if let Some(callback) = props_on_click.clone() {
+                callback.emit(event);
+            }
+        })
+    };
+
     html! {
         // do not specify button type. Blocks onsubmit if part of a form.
-        <button> {props.label.clone()} </button>
+        <button onclick={on_click_callback}> {props.label.clone()} </button>
     }
 }
