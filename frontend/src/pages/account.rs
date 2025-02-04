735c73681be62237f77b8c1ad713fc582bbb2a41
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2025-02-04 16:53:32 +1100

    Add refresh list button to logged in devices info/token list. Stop continual requests and only request on new button push

--- frontend/src/pages/account.rs
+++ frontend/src/pages/account.rs
@@ -34,13 +34,14 @@ pub fn account_management_page() -> Html {
     }
 
     // Component state
-    let token_info = use_state(|| Vec::<LoginTokenInfo>::new());
+    let token_info = use_state_eq(|| Vec::<LoginTokenInfo>::new());
 
     // Update token_info state if needed
     if let Some(user_data) = store.user.clone() {
         let token_info = token_info.clone();
+        let user_token = user_data.token;
         wasm_bindgen_futures::spawn_local(async move {
-            if let Ok(info) = api_service::account_get_active_token_info(&user_data.token).await {
+            if let Ok(info) = api_service::account_get_active_token_info(&user_token).await {
                 token_info.set(info);
             }
         })
@@ -50,6 +51,25 @@ pub fn account_management_page() -> Html {
     let token_children_info: Vec<Html> = token_info.iter()
         .map(|info| html! { <TokenInfo info={info.clone()}/>})
         .collect();
+
+    let on_refresh_tokens = {
+        let token_info = token_info.clone();
+        let store = store.clone();
+        Callback::from(move |_: MouseEvent| {
+            let token_info = token_info.clone();
+            let store = store.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let store = store.clone();
+                let user_token = match &store.user {
+                    Some(user) => user.token,
+                    None => return
+                };
+                if let Ok(info) = api_service::account_get_active_token_info(&user_token).await {
+                    token_info.set(info);
+                }
+            })
+        })
+    };
 
     let on_clear_tokens = {
         let navigator = navigator.clone();
@@ -107,6 +127,7 @@ pub fn account_management_page() -> Html {
     html! {
         <>
             <h>{"Currently logged in devices/tokens"}</h>
+            <Button label={"Refresh list"} on_click={Some(on_refresh_tokens)} />
             <ListView children={token_children_info} />
             <Button label={"Change password"} on_click={Some(on_change_password)} />
             <br />
